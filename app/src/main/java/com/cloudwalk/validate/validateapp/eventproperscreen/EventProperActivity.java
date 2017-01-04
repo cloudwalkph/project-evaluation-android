47f5cb2d3ffcb8bdef08acbce6be875246c0d3eb
Nina Kristel Cheng <nkmcheng@live.com>
2017-01-04 13:21:41 +0800

    added onclick to pre event survey

--- app/src/main/java/com/cloudwalk/validate/validateapp/eventproperscreen/EventProperActivity.java
+++ app/src/main/java/com/cloudwalk/validate/validateapp/eventproperscreen/EventProperActivity.java
@@ -1,11 +1,15 @@
 package com.cloudwalk.validate.validateapp.eventproperscreen;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.cloudwalk.validate.validateapp.R;
 import com.cloudwalk.validate.validateapp.data.local.models.Event;
+import com.cloudwalk.validate.validateapp.preeventsurveyscreen.PreEventSurveyActivity;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
@@ -16,12 +20,23 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 public class EventProperActivity extends AppCompatActivity {
 
     public static Event mCurrentEvent;
+    public LinearLayout mPreEventLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_proper);
         setTitle(mCurrentEvent.getName());
+
+        mPreEventLayout = (LinearLayout) findViewById(R.id.ll_pre_event);
+        mPreEventLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PreEventSurveyActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
