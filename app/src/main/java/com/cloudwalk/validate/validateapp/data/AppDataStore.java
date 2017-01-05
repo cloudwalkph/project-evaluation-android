862c12626f1a9485b17848246d8513e35969a2f5
Nina Kristel Cheng <nkmcheng@live.com>
2017-01-05 12:11:41 +0800

    added content provider in manifest

--- app/src/main/java/com/cloudwalk/validate/validateapp/data/AppDataStore.java
+++ app/src/main/java/com/cloudwalk/validate/validateapp/data/AppDataStore.java
@@ -1,8 +1,11 @@
 package com.cloudwalk.validate.validateapp.data;
 
+import java.util.Observable;
+
 /**
  * Created by nkmcheng on 03/01/2017.
  */
 
 public interface AppDataStore {
+    Observable getPost();
 }
