9c161cf9bc35f479a5f9964f5f88167d3c7f6555
Nina Kristel Cheng <nkmcheng@live.com>
2017-01-05 13:32:22 +0800

    updated modules and manifest

--- app/src/main/java/com/cloudwalk/validate/validateapp/dagger/module/AppModule.java
+++ app/src/main/java/com/cloudwalk/validate/validateapp/dagger/module/AppModule.java
@@ -3,12 +3,25 @@ package com.cloudwalk.validate.validateapp.dagger.module;
 import android.app.Application;
 
 import javax.inject.Singleton;
+
+import dagger.Module;
 import dagger.Provides;
 
 /**
  * Created by nkmcheng on 03/01/2017.
  */
 
+@Module
 public class AppModule {
+    Application mApplication;
 
+    public AppModule(Application mApplication) {
+        this.mApplication = mApplication;
+    }
+
+    @Provides
+    @Singleton
+    Application provideApplication() {
+        return mApplication;
+    }
 }
--- app/src/main/java/com/cloudwalk/validate/validateapp/dagger/module/DataModule.java
+++ app/src/main/java/com/cloudwalk/validate/validateapp/dagger/module/DataModule.java
@@ -1,9 +1,90 @@
 package com.cloudwalk.validate.validateapp.dagger.module;
 
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.cloudwalk.validate.validateapp.data.local.AppLocalDataStore;
+import com.cloudwalk.validate.validateapp.data.remote.AppRemoteDataStore;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by nkmcheng on 03/01/2017.
  */
 
+@Module
 public class DataModule {
+    String mBaseUrl;
+
+    public DataModule(String mBaseUrl) {
+        this.mBaseUrl = mBaseUrl;
+    }
+
+    @Provides
+    @Singleton
+    SharedPreferences providesSharedPreferences(Application application) {
+        return PreferenceManager.getDefaultSharedPreferences(application);
+    }
+
+    @Provides
+    @Singleton
+    Cache provideHttpCache(Application application) {
+        int cacheSize = 10 * 1024 * 1024;
+        Cache cache = new Cache(application.getCacheDir(), cacheSize);
+        return cache;
+    }
+
+    @Provides
+    @Singleton
+    Gson provideGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+        return gsonBuilder.create();
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkhttpClient(Cache cache) {
+        OkHttpClient.Builder client = new OkHttpClient.Builder();
+        client.cache(cache);
+        return client.build();
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(Gson gson, OkHttpClient okHttpClient) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .baseUrl(mBaseUrl)
+                .client(okHttpClient)
+                .build();
+        return retrofit;
+    }
+
+
+    @Provides
+    @Singleton
+    AppLocalDataStore porvidesAppLocalDataStore(Application context) {
+        return new AppLocalDataStore(context);
+    }
+
+    @Provides
+    @Singleton
+    AppRemoteDataStore providesRepository() {
+        return new AppRemoteDataStore();
+    }
 
 }
