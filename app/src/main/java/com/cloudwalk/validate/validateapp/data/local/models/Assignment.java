ef9b25e4060c8070984450bc3fd5f6954e8873cf
Nina Kristel Cheng <nkmcheng@live.com>
2017-01-10 17:36:28 +0800

    updated assignment model for sync

--- app/src/main/java/com/cloudwalk/validate/validateapp/data/local/models/Assignment.java
+++ app/src/main/java/com/cloudwalk/validate/validateapp/data/local/models/Assignment.java
@@ -11,43 +11,38 @@ import com.pushtorefresh.storio.sqlite.annotations.StorIOSQLiteType;
  * Created by nkmcheng on 09/01/2017.
  */
 
-//@StorIOSQLiteType(table = AssignmentDatabaseContract.Assignment.TABLE_NAME)
-//@StorIOContentResolverType(uri = AssignmentDatabaseContract.Assignment.CONTENT_URI_STRING)
+@StorIOSQLiteType(table = AssignmentDatabaseContract.Assignment.TABLE_NAME)
+@StorIOContentResolverType(uri = AssignmentDatabaseContract.Assignment.CONTENT_URI_STRING)
 public class Assignment {
 
-//    @StorIOSQLiteColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_ID, key = true)
-//    @StorIOContentResolverColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_ID, key = true)
-//    @SerializedName("_id")
+    @StorIOSQLiteColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_ID, key = true)
+    @StorIOContentResolverColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_ID, key = true)
+    @SerializedName("_id")
     protected long id;
 
-//    @StorIOSQLiteColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_DEPARTMENT)
-//    @StorIOContentResolverColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_DEPARTMENT)
-//    @SerializedName("dept")
-    protected String department;
-
-//    @StorIOSQLiteColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QNUM)
-//    @StorIOContentResolverColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QNUM)
-//    @SerializedName("qnum")
+    @StorIOSQLiteColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QNUM)
+    @StorIOContentResolverColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QNUM)
+    @SerializedName("qnum")
     protected String qnum;
 
-//    @StorIOSQLiteColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QCAT)
-//    @StorIOContentResolverColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QCAT)
-//    @SerializedName("qcat")
+    @StorIOSQLiteColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QCAT)
+    @StorIOContentResolverColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QCAT)
+    @SerializedName("qcat")
     protected String qcat;
 
-//    @StorIOSQLiteColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QRATER)
-//    @StorIOContentResolverColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QRATER)
-//    @SerializedName("qrater")
+    @StorIOSQLiteColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QRATER)
+    @StorIOContentResolverColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QRATER)
+    @SerializedName("rater")
     protected String qrater;
 
-//    @StorIOSQLiteColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QRATEE)
-//    @StorIOContentResolverColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QRATEE)
-//    @SerializedName("qratee")
+    @StorIOSQLiteColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QRATEE)
+    @StorIOContentResolverColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QRATEE)
+    @SerializedName("ratee")
     protected String qratee;
 
-//    @StorIOSQLiteColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QEVENT)
-//    @StorIOContentResolverColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QEVENT)
-//    @SerializedName("qevent")
+    @StorIOSQLiteColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QEVENT)
+    @StorIOContentResolverColumn(name = AssignmentDatabaseContract.Assignment.COLUMN_QEVENT)
+    @SerializedName("qevent")
     protected String qevent;
 
 
@@ -57,14 +52,6 @@ public class Assignment {
 
     public void setId(long id) {
         this.id = id;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        department = department;
     }
 
     public String getQnum() {
