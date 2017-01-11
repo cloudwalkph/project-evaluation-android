96514cb4e08597dd0daf93ae09513370a4df93b8
Nina Kristel Cheng <nkmcheng@live.com>
2017-01-11 18:03:10 +0800

    added login in login activity

--- app/src/main/java/com/cloudwalk/validate/validateapp/loginscreen/LoginActivity.java
+++ app/src/main/java/com/cloudwalk/validate/validateapp/loginscreen/LoginActivity.java
@@ -5,20 +5,26 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.cloudwalk.validate.validateapp.R;
 import com.cloudwalk.validate.validateapp.eventproperscreen.EventProperActivity;
 import com.cloudwalk.validate.validateapp.mainscreen.MainActivity;
 
+import butterknife.Bind;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class LoginActivity extends AppCompatActivity
     implements LoginScreenContract.View {
 
     public Button mBtnSignIn;
+
+    @Bind(R.id.email_text) EditText mEmail;
+    @Bind(R.id.password_text) EditText mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,7 @@ public class LoginActivity extends AppCompatActivity
     private void initSigninButton() {
         mBtnSignIn = (Button) findViewById(R.id.btn_log_in);
         mBtnSignIn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
--- app/src/main/java/com/cloudwalk/validate/validateapp/loginscreen/LoginScreenPresenter.java
+++ app/src/main/java/com/cloudwalk/validate/validateapp/loginscreen/LoginScreenPresenter.java
@@ -1,6 +1,15 @@
 package com.cloudwalk.validate.validateapp.loginscreen;
 
+import android.util.Log;
+
+import com.cloudwalk.validate.validateapp.data.AppRepository;
+import com.cloudwalk.validate.validateapp.data.local.models.Employee;
+import com.cloudwalk.validate.validateapp.data.local.models.Event;
+
+import rx.Observer;
 import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by alleoindong on 1/5/17.
@@ -8,17 +17,39 @@ import rx.Subscription;
 
 public class LoginScreenPresenter implements LoginScreenContract.Presenter {
     private static final String TAG = LoginScreenPresenter.class.getSimpleName();
+    public static Employee mCurrentEmployee;
     private Subscription mSubscription;
+    private AppRepository mAppRepository;
     private LoginScreenContract.View mView;
 
     public LoginScreenPresenter(LoginScreenContract.View mView) {
+        this.mAppRepository = mAppRepository;
         this.mView = mView;
         this.mView.setPresenter(this);
     }
 
     @Override
     public void attemptLogin(String email, String password) {
+        mSubscription = mAppRepository.attemptLogin(email, password)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.newThread())
+                .subscribe(new Observer<Employee>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d("LOGIN", "Successfully logged in");
+                    }
 
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d("LOGIN", e.toString());
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onNext(Employee employee) {
+                        mCurrentEmployee = employee;
+                    }
+                });
     }
 
     @Override
@@ -28,6 +59,7 @@ public class LoginScreenPresenter implements LoginScreenContract.Presenter {
 
     @Override
     public void unsubscribe() {
-
+        if (mSubscription != null && mSubscription.isUnsubscribed())
+            mSubscription.unsubscribe();
     }
 }
