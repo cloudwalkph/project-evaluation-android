8405ab3f48a415db7dd49b5872b49493f18ef522
Nina Kristel Cheng <nkmcheng@live.com>
2017-01-20 04:41:13 +0800

    updated record model

--- app/src/main/java/com/cloudwalk/validate/validateapp/data/local/models/Record.java
+++ app/src/main/java/com/cloudwalk/validate/validateapp/data/local/models/Record.java
@@ -1,6 +1,8 @@
 package com.cloudwalk.validate.validateapp.data.local.models;
 
 import com.cloudwalk.validate.validateapp.data.local.EventDatabaseContract;
+import com.cloudwalk.validate.validateapp.data.local.QuestionDatabaseContract;
+import com.cloudwalk.validate.validateapp.data.local.RecordDatabaseContract;
 import com.google.gson.annotations.SerializedName;
 import com.pushtorefresh.storio.contentresolver.annotations.StorIOContentResolverColumn;
 import com.pushtorefresh.storio.contentresolver.annotations.StorIOContentResolverType;
@@ -10,28 +12,63 @@ import com.pushtorefresh.storio.sqlite.annotations.StorIOSQLiteType;
 /**
  * Created by nkmcheng on 03/01/2017.
  */
+
+@StorIOSQLiteType(table = RecordDatabaseContract.Record.TABLE_NAME)
+@StorIOContentResolverType(uri = RecordDatabaseContract.Record.CONTENT_URI_STRING)
 public class Record {
+
+    @StorIOSQLiteColumn(name = RecordDatabaseContract.Record.COLUMN_ID, key = true)
+    @StorIOContentResolverColumn(name = RecordDatabaseContract.Record.COLUMN_ID, key = true)
     @SerializedName("_id")
     protected long id;
 
+    @StorIOSQLiteColumn(name = RecordDatabaseContract.Record.COLUMN_EVENT)
+    @StorIOContentResolverColumn(name = RecordDatabaseContract.Record.COLUMN_EVENT)
     @SerializedName("eid")
     protected String eventId;
 
+    @StorIOSQLiteColumn(name = RecordDatabaseContract.Record.COLUMN_CATEGORY)
+    @StorIOContentResolverColumn(name = RecordDatabaseContract.Record.COLUMN_CATEGORY)
     @SerializedName("qcat")
     protected String category;
 
+    @StorIOSQLiteColumn(name = RecordDatabaseContract.Record.COLUMN_QEVENT)
+    @StorIOContentResolverColumn(name = RecordDatabaseContract.Record.COLUMN_QEVENT)
     @SerializedName("qevent")
     protected String qEvent;
 
+    @StorIOSQLiteColumn(name = RecordDatabaseContract.Record.COLUMN_QNUM)
+    @StorIOContentResolverColumn(name = RecordDatabaseContract.Record.COLUMN_QNUM)
     @SerializedName("qid")
     protected String questionId;
 
+    @StorIOSQLiteColumn(name = RecordDatabaseContract.Record.COLUMN_ANSWER)
+    @StorIOContentResolverColumn(name = RecordDatabaseContract.Record.COLUMN_ANSWER)
     @SerializedName("qans")
     protected String answerId;
 
+    @StorIOSQLiteColumn(name = RecordDatabaseContract.Record.COLUMN_TLID)
+    @StorIOContentResolverColumn(name = RecordDatabaseContract.Record.COLUMN_TLID)
     protected String tlid;
 
+    @StorIOSQLiteColumn(name = RecordDatabaseContract.Record.COLUMN_NEGOID)
+    @StorIOContentResolverColumn(name = RecordDatabaseContract.Record.COLUMN_NEGOID)
     protected String negoid;
+
+    public Record() {
+    }
+
+    public Record(long id, String eventId, String category,
+                  String qEvent, String questionId, String answerId, String tlid, String negoid) {
+        this.id = id;
+        this.eventId = eventId;
+        this.category = category;
+        this.qEvent = qEvent;
+        this.questionId = questionId;
+        this.answerId = answerId;
+        this.tlid = tlid;
+        this.negoid = negoid;
+    }
 
     public long getId() {
         return id;
